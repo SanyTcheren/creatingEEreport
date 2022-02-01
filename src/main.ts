67d5eceace8dec9682f9d73ed23418affefd39f0
SanyTcheren <tcherenkovskiy@gmail.com>
2022-02-01 17:35:45 +0500

    add simple frontend

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
 
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req: Request, res: Response) => {
-	res.send('Hello from Sany!\n');
+// КОНФИГУРАЦИЯ ПРИЛОЖЕНИЯ
+// ==================================================
+// сообщаем Node где лежат ресурсы сайта
+app.use(express.static(path.join(__dirname, '../public')));
+
+// устанавливаем движок EJS для представления
+app.set('view engine', 'ejs');
+
+app.get('/login', function (req, res) {
+	res.render('pages/login');
+});
+
+app.get('/register', function (req, res) {
+	res.render('pages/register');
+});
+
+app.get('/input', function (req, res) {
+	res.render('pages/input');
+});
+
+app.post('/report', function (req, res) {
+	res.render('pages/report');
 });
 
 app.listen(port, () => {
-	console.log(`Server started on http://localhost:${port}/hello`);
+	console.log(`Server started on http://localhost:${port}/login`);
 });
