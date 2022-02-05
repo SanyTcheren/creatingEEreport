2637724ae29d0307489ef55734d27d88750fe28a
SanyTcheren <tcherenkovskiy@gmail.com>
2022-02-05 23:31:09 +0500

    add write start and end

--- src/report/util/reportBilder.ts
+++ src/report/util/reportBilder.ts
@@ -7,6 +7,7 @@ import Excel from 'exceljs';
 import { OilWell } from '../oilwell';
 import moment, { Moment } from 'moment';
 import { getPower } from './readPowerProfile';
+import { WellPrepareDrill } from '../../types/custom';
 
 @injectable()
 export class ReportBuilder {
@@ -25,7 +26,7 @@ export class ReportBuilder {
 		'декабрь',
 	];
 
-	keys = {
+	ROWS = {
 		maxWells: 4,
 		maxRow: 21,
 		firstRow: 10,
@@ -66,7 +67,7 @@ export class ReportBuilder {
 			const sheet = workbook.getWorksheet(1);
 
 			//получаем данные для отчета
-			const wells = await this.getSortedWells();
+			const wells = await this.getWellPrepareDrill();
 			const { power, year, month } = await getPower(powerPath);
 			this.year = year;
 			this.month = month;
@@ -84,16 +85,28 @@ export class ReportBuilder {
 		return resultPath;
 	}
 
-	async getSortedWells(): Promise<OilWell[]> {
+	async getWellPrepareDrill(): Promise<WellPrepareDrill[]> {
 		const wells = this.report.wells;
 		if (wells.length == 0) return [];
 
-		const sortedWells = Object.values(wells).sort((a, b) => {
+		const sortedWells: any[] = Object.values(wells).sort((a, b) => {
 			const aStart = moment(a.start);
 			const bStart = moment(b.start);
 			return aStart.isAfter(bStart) ? 1 : -1;
 		});
-		return sortedWells;
+		//Нормализуем данные по скважинам так, что первый элемент массива - prepare, а последний - drill
+		if (sortedWells[0].detail == 'drill') {
+			sortedWells.unshift(null);
+		}
+		if (sortedWells[sortedWells.length - 1].detail == 'prepare') {
+			sortedWells.push(null);
+		}
+		//Сгруппируем данные по скважинам
+		const result: WellPrepareDrill[] = [];
+		for (let i = 0; i < sortedWells.length; i += 2) {
+			result.push({ prepare: sortedWells[i], drill: sortedWells[i + 1] });
+		}
+		return result;
 	}
 
 	//Возвращаем двумерный массив 31*2, соответствующий часам работы на каждый день, -1 - нерабочий день 23 - полный рабочий день
@@ -130,95 +143,88 @@ export class ReportBuilder {
 		return hours;
 	}
 
-	setSheet = async (sheet: Excel.Worksheet, wells: OilWell[], power: number[][]): Promise<void> => {
+	setSheet = async (
+		sheet: Excel.Worksheet,
+		wells: WellPrepareDrill[],
+		power: number[][],
+	): Promise<void> => {
 		// Устанавливаем общие данные
-		// const data = await readData();
 		sheet.getCell(this.CELLS.month).value = this.MONTH[this.month - 1];
 		sheet.getCell(this.CELLS.year).value = `Месяц ${this.year} года`;
 		sheet.getCell(this.CELLS.typeNumber).value = `${this.report.type}, зав №${this.report.number}`;
 		sheet.getCell(this.CELLS.field).value = this.report.field;
 		sheet.getCell(this.CELLS.bush).value = this.report.bush;
 
-		//Срезаем лишние строки из таблицы
-		// const sliceCount = (keys.maxWells - wells.length) * 3;
-		// const endRow = keys.maxRow - sliceCount + 1;
-		// sheet.spliceRows(endRow, sliceCount);
-
-		//Восстанавливем свойства объединенных ячеек
-		// sheet.unMergeCells(`B${28 - sliceCount}:D${28 - sliceCount}`);
-		// sheet.mergeCells(`B${28 - sliceCount}:D${28 - sliceCount}`);
-		// sheet.unMergeCells(`B${31 - sliceCount}:D${31 - sliceCount}`);
-		// sheet.mergeCells(`B${31 - sliceCount}:D${31 - sliceCount}`);
-		// sheet.unMergeCells(`B${34 - sliceCount}:D${34 - sliceCount}`);
-		// sheet.mergeCells(`B${34 - sliceCount}:D${34 - sliceCount}`);
-		// sheet.unMergeCells(`B${37 - sliceCount}:D${37 - sliceCount}`);
-		// sheet.mergeCells(`B${37 - sliceCount}:D${37 - sliceCount}`);
-		// sheet.unMergeCells(`B${38 - sliceCount}:D${38 - sliceCount}`);
-		// sheet.mergeCells(`B${38 - sliceCount}:D${38 - sliceCount}`);
-		// sheet.unMergeCells(`B${40 - sliceCount}:D${40 - sliceCount}`);
-		// sheet.mergeCells(`B${40 - sliceCount}:D${40 - sliceCount}`);
-		// sheet.unMergeCells(`L${32 - sliceCount}:P${32 - sliceCount}`);
-		// sheet.mergeCells(`L${32 - sliceCount}:P${32 - sliceCount}`);
-		// sheet.unMergeCells(`L${35 - sliceCount}:P${35 - sliceCount}`);
-		// sheet.mergeCells(`L${35 - sliceCount}:P${35 - sliceCount}`);
-		// sheet.unMergeCells(`AH${22 - sliceCount}:AI${22 - sliceCount}`);
-		// sheet.mergeCells(`AH${22 - sliceCount}:AI${22 - sliceCount}`);
-
-		// const month = await getKeyValue(PARAMS.month);
-		// const year = await getKeyValue(PARAMS.year);
-
-		//Индекс последней строки
-		// const lastRow = this.keys.firstRow + wells.length * 3;
 		//Заполняем данные по скважинам
-		// for (let i = 0; i < wells.length; i++) {
-		// 	const number = (wells[i][wells[i].prepare ? 'prepare' : 'build'].number);
-		// 	sheet.getCell(`A${10 + 3 * i}`).value = number;
-		// 	//заполняем пзр и бурение одинаково, 0- пзр, 1 - бурение
-		// 	for (let k = 0; k < keys.stage.length; k++) {
-		// 		if (wells[i][keys.stage[k]]) {
-		// 			//начало работ
-		// 			let start = wells[i][keys.stage[k]].start
-		// 			sheet.getCell(`C${keys.firstRow + k + 3 * i}`).value = start;
-		// 			start = moment(start);
-		// 			//окончание работ
-		// 			let end = wells[i][keys.stage[k]].end;
-		// 			sheet.getCell(`D${keys.firstRow + k + 3 * i}`).value = /^3000.*/.test(end) ? 'переход' : end;
-		// 			end = moment(end);
-		// 			//потребленная мощность
-		// 			const hours = getHoursOfWork(start, end, month, year);
-		// 			for (let d = 0; d < hours.length; d++) { //d - будет соответствовать (дню месяца - 1)
-		// 				if (hours[d][0] == -1) continue;
-		// 				//отрезаем нужный массив энергии и суммируем
-		// 				let pow = power[d].slice(hours[d][0], hours[d][1] + 1).reduce((s, p) => s + p);
-		// 				sheet.getCell(`${keys.day[d]}${keys.firstRow + k + 3 * i}`).value = Math.round(pow);
-		// 			}
-		// 		}
-		// 	}
-		//Запись математичиских формул
-		// for (let m = 0; m < keys.day.length; m++) {
-		// 	sheet.getCell(`${keys.day[m]}${keys.firstRow + 2 + 3 * i}`).value = {
-		// 		formula: `${keys.day[m]}${keys.firstRow + 3 * i}+${keys.day[m]}${keys.firstRow + 1 + 3 * i}`
-		// 	}
-		// }
-		// sheet.getCell(`AJ${keys.firstRow + i * 3}`).value = {
-		// 	formula: `SUM(E${keys.firstRow + i * 3}:AI${keys.firstRow + i * 3})`,
-		// }
-		// sheet.getCell(`AJ${keys.firstRow + 1 + i * 3}`).value = {
-		// 	formula: `SUM(E${keys.firstRow + 1 + i * 3}:AI${keys.firstRow + 1 + i * 3})`,
-		// }
-		// sheet.getCell(`AJ${keys.firstRow + 2 + i * 3}`).value = {
-		// 	formula: `AJ${keys.firstRow + i * 3}+AJ${keys.firstRow + 1 + i * 3}`,
-		// }
+		for (let i = 0; i < wells.length; i++) {
+			//заполняем пзр и бурение одинаково, 0- пзр, 1 - бурение
+			const keys = {
+				prepare: 0,
+				drill: 1,
+			};
+			let detail: 'prepare' | 'drill';
+			for (detail in keys) {
+				if (wells[i][detail]) {
+					//Номер скважины
+					sheet.getCell(`A${10 + 3 * i}`).value = wells[i][detail]?.well;
+					//начало работ
+					const St = wells[i][detail]?.start;
+					sheet.getCell(`C${this.ROWS.firstRow + keys[detail] + 3 * i}`).value = St;
+					const start: Moment = moment(St);
+					//окончание работ
+					const End = wells[i][detail]?.end;
+					sheet.getCell(`D${this.ROWS.firstRow + keys[detail] + 3 * i}`).value = End;
+					const end: Moment = moment(End);
+				}
 
-		// sheet.getCell(`AK${keys.firstRow + i * 3}`).value = {
-		// 	formula: `AJ${keys.firstRow + i * 3}/AJ${lastRow}*AL${lastRow}`,
-		// }
-		// sheet.getCell(`AK${keys.firstRow + 1 + i * 3}`).value = {
-		// 	formula: `AJ${keys.firstRow + 1 + i * 3}/AJ${lastRow}*AL${lastRow}`,
-		// }
-		// sheet.getCell(`AK${keys.firstRow + 2 + i * 3}`).value = {
-		// 	formula: `AK${keys.firstRow + i * 3}+AK${keys.firstRow + 1 + i * 3}`,
-		// }
-		// }
+				// const number = wells[i].well;
+				// sheet.getCell(`A${10 + 3 * i}`).value = number;
+				// 	//заполняем пзр и бурение одинаково, 0- пзр, 1 - бурение
+				// 	for (let k = 0; k < keys.stage.length; k++) {
+				// 		if (wells[i][keys.stage[k]]) {
+				// 			//начало работ
+				// 			let start = wells[i][keys.stage[k]].start
+				// 			sheet.getCell(`C${keys.firstRow + k + 3 * i}`).value = start;
+				// 			start = moment(start);
+				// 			//окончание работ
+				// 			let end = wells[i][keys.stage[k]].end;
+				// 			sheet.getCell(`D${keys.firstRow + k + 3 * i}`).value = /^3000.*/.test(end) ? 'переход' : end;
+				// 			end = moment(end);
+				// 			//потребленная мощность
+				// 			const hours = getHoursOfWork(start, end, month, year);
+				// 			for (let d = 0; d < hours.length; d++) { //d - будет соответствовать (дню месяца - 1)
+				// 				if (hours[d][0] == -1) continue;
+				// 				//отрезаем нужный массив энергии и суммируем
+				// 				let pow = power[d].slice(hours[d][0], hours[d][1] + 1).reduce((s, p) => s + p);
+				// 				sheet.getCell(`${keys.day[d]}${keys.firstRow + k + 3 * i}`).value = Math.round(pow);
+				// 			}
+				// 		}
+			}
+			//Запись математичиских формул
+			// for (let m = 0; m < keys.day.length; m++) {
+			// 	sheet.getCell(`${keys.day[m]}${keys.firstRow + 2 + 3 * i}`).value = {
+			// 		formula: `${keys.day[m]}${keys.firstRow + 3 * i}+${keys.day[m]}${keys.firstRow + 1 + 3 * i}`
+			// 	}
+			// }
+			// sheet.getCell(`AJ${keys.firstRow + i * 3}`).value = {
+			// 	formula: `SUM(E${keys.firstRow + i * 3}:AI${keys.firstRow + i * 3})`,
+			// }
+			// sheet.getCell(`AJ${keys.firstRow + 1 + i * 3}`).value = {
+			// 	formula: `SUM(E${keys.firstRow + 1 + i * 3}:AI${keys.firstRow + 1 + i * 3})`,
+			// }
+			// sheet.getCell(`AJ${keys.firstRow + 2 + i * 3}`).value = {
+			// 	formula: `AJ${keys.firstRow + i * 3}+AJ${keys.firstRow + 1 + i * 3}`,
+			// }
+
+			// sheet.getCell(`AK${keys.firstRow + i * 3}`).value = {
+			// 	formula: `AJ${keys.firstRow + i * 3}/AJ${lastRow}*AL${lastRow}`,
+			// }
+			// sheet.getCell(`AK${keys.firstRow + 1 + i * 3}`).value = {
+			// 	formula: `AJ${keys.firstRow + 1 + i * 3}/AJ${lastRow}*AL${lastRow}`,
+			// }
+			// sheet.getCell(`AK${keys.firstRow + 2 + i * 3}`).value = {
+			// 	formula: `AK${keys.firstRow + i * 3}+AK${keys.firstRow + 1 + i * 3}`,
+			// }
+		}
 	};
 }
--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,7 +1,19 @@
+import { OilWell } from '../report/oilwell';
+
 export type Detail = 'drill' | 'prepare';
 
 export interface PowerYearMonth {
 	power: number[][];
 	year: number;
 	month: number;
+}
+
+export interface WellPrepareDrill {
+	prepare: OilWell | null;
+	drill: OilWell | null;
+}
+
+export interface keysPrepare {
+	prepare: 0;
+	drill: 1;
 }
